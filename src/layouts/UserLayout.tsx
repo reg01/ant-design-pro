8dc04e9f039afaa869e268cc9e9a2190d8ac0bc1
js_82Wita <30684547+reg01@users.noreply.github.com>
2021-01-10 17:56:03 +0800

    改标题

--- src/layouts/UserLayout.tsx
+++ src/layouts/UserLayout.tsx
@@ -49,13 +49,13 @@ const UserLayout: React.FC<UserLayoutProps> = (props) => {
             <div className={styles.header}>
               <Link to="/">
                 <img alt="logo" className={styles.logo} src={logo} />
-                <span className={styles.title}>Ant Design</span>
+                <span className={styles.title}>云塑 Y5</span>
               </Link>
             </div>
             <div className={styles.desc}>
               <FormattedMessage
                 id="pages.layouts.userLayout.title"
-                defaultMessage="Ant Design 是西湖区最具影响力的 Web 设计规范"
+                defaultMessage="云原生 塑未来"
               />
             </div>
           </div>
